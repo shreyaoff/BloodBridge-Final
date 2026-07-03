29c1efdd1f07ec11d11ab84925bbc44bf60e9130
Shreya Dhakal <shreyaaoff60@gmail.com>
2026-07-03 14:46:35 +0545

    edited

--- js/search.js
+++ js/search.js
@@ -58,8 +58,8 @@ fetch("../data/hospitals.json")
       var h = hospitals[i];
       var card = "<div class='hospital-card'>" +
         "<h3>" + h.name + "</h3>" +
-        "<p>📍 " + h.address + "</p>" +
-        "<p>📞 " + h.contact + "</p>" +
+        "<p> " + h.address + "</p>" +
+        "<p> " + h.contact + "</p>" +
         "<span class='hospital-type'>" + h.type + "</span>" +
         "</div>";
       $("#hospitalsGrid").append(card);
